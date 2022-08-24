42ea7ef6d55e26c01531ad4fea749ec348065810
fauzinurfariyanto <fauzinur000@gmail.com>
2022-08-24 16:39:09 +0700

    Transform, Transition, Animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,17 @@ module.exports = {
         core: '#3498db',
         alizrin: '#e74c3c',
         midnight : '#2c3e50',
-      }
+      },
+      animation: {
+        'spin': 'spin 3s linear infinite',
+        'wiggle': 'wiggle 1s ease-in-out infinite',
+      },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        }
+      },
     },
   },
   plugins: [],
